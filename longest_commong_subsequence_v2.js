5976033cb8a36aefbb8f50594ecd41a4e5b3a59c
Jake McBride <pinindajin@gmail.com>
2019-09-04 21:41:54 -0600

    Create longest_commong_subsequence_v2.js

---
+++ longest_commong_subsequence_v2.js
@@ -0,0 +1,56 @@
+/**
+ * PROBLEM:
+ * 
+ * When Alex is preparing for his next climb and has decided that the 
+ * best way to prepare is to write down two different unique sequences
+ * of moves he can do to complete his route. Alex has gives each individual 
+ * move within a given unique sequence a code name. 
+ * 
+ * An example of a unique sequence of moves could be represented as an array of 
+ * strings such as the following: ["Sambal", "Olec", "Pretty", "Bird", "Hunger", "Totem"]
+ * 
+ * After having written down two separate unique sequences,
+ * Alex has then finds the longest common subsequence among
+ * the two unique sequences so that he can put extra practice
+ * into those moves.
+ * 
+ * Alex loves climbing, but does not really like having to do logic puzzles.
+ * Fortunately you are a friend of Alex and have agreed to write some code
+ * to figure out the longest common subsequence among two unique sequences
+ * for him.
+ * 
+ * EXAMPLE CASES
+ * 
+ * ['Dog', 'Yam', 'Dog', 'Tattoo', 'Ginger', 'Shoe']
+ * ['Yam', 'Dog', 'Shoe', 'Yam', 'Dog', 'Ginger']
+ * => 
+ * ['Dog', 'Yam', 'Dog', 'Ginger']
+ * 
+ * 
+ * ['Dog', 'Ludacious', 'Ludacious', 'Anakin', 'Dog', 'Yam']
+ * ['Ludacious', 'Spyro', 'Anakin', 'Spyro', 'Dog', 'Plum', 'Yam'] 
+ * =>
+ * ['Ludacious', 'Anakin', 'Dog', 'Yam']
+ * 
+ * 
+ * ['Dog', 'Dog', 'Dog', 'Dog']
+ * ['Dog', 'Dog']
+ * =>
+ * ['Dog', 'Dog']
+ * 
+ * 
+ * ['Tea', 'Tea', 'Tea']
+ * []
+ * => 
+ * []
+ * 
+ * 
+ * ['Dog', 'Yam', 'Yam', 'Dog']
+ * ['Dog', 'Yam', 'Trout', 'Dog', 'Yam', 'Dog'] 
+ * => 
+ * ['Dog', 'Yam', 'Yam', 'Dog']
+ */
+
+const longestCommonSubsequence = (sequenceA, sequenceB) => {
+    // TODO
+}
